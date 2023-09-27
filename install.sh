b90d87910d050b095df282b02dde446b8ac82c34
Ram <lliill.education.lliill@gmail.com>
2023-09-27 12:10:19 +0100

    [feat] add tmux and its configs

--- install.sh
+++ install.sh
@@ -12,16 +12,15 @@ echo -e "================= INSTALLING ===================="
 echo -e "================================================="
 # Configurations __________________________________________#
 
-cp ./configs/SHELL/bash/bashrc ~/.bashrc
-cp ./configs/EDITOR/vim/vimrc ~/.vimrc 
+cp -f ./configs/SHELL/bash/bashrc ~/.bashrc
+cp -f ./configs/EDITOR/vim/vimrc ~/.vimrc 
 mkdir -p ~/.vim
-cp -r ./configs/EDITOR/vim/colors ~/.vim
-mkdir -p ~/.config/alacritty/
-cp ./configs/TERMINAL/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+cp -rf ./configs/EDITOR/vim/colors ~/.vim/
+cp -rf ./configs/TERMINAL/alacritty/ ~/.config/
 mkdir -p ~/.config/xmonad
-cp ./configs/WM/xmonad/xmonad.hs ~/.config/xmonad/xmonad.hs
-mkdir -p ~/.config/nvim
-cp -r ./configs/EDITOR/nvim ~/.config/
+cp -f ./configs/WM/xmonad/xmonad.hs ~/.config/xmonad/xmonad.hs
+cp -rf ./configs/EDITOR/nvim ~/.config/
+cp -rf ./configs/TERMINAL/tmux ~/.config/
 
 echo -e "${cyan}"
 echo "  bashrc ✓" && sleep 0.3
@@ -29,12 +28,13 @@ echo "  vimrc ✓" && sleep 0.3
 echo "  alacritty ✓" && sleep 0.3
 echo "  xmonad ✓" && sleep 0.3
 echo "  neovim ✓" && sleep 0.3
+echo "  tmux ✓" && sleep 0.3
 echo -e "${reset}"
 #__________________________________________________________#
 
 ## Downloads _______________________________________________#
 echo -e "${red} Downloading Packages ! ${reset}" && sleep 0.3 
-#sudo pacman -Syy neofetch vim neovim git lsd alacritty evince tree htop 
-
-
+#sudo pacman -Syy neofetch vim neovim git lsd alacritty evince tree htop tmux
+#git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+#git clone --depth 1 https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 echo -e "${green} Done !!!!${reset}"
