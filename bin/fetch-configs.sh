3db3943855aba36375c7f17cd886962f00c15a7a
NEW_NAME <NEW_EMAIL>
2025-03-12 20:26:28 +0100

    [backup]

--- bin/fetch-configs.sh
+++
@@ -1,7 +0,0 @@
-CONFIGS=$(find ~ -type d -path "*/myArch/configs")
-
-cp -f $CONFIGS/bashrc ~/.bashrc
-cp -f $CONFIGS/vim/vimrc ~/.vimrc 
-mkdir -p ~/.vim
-cp -rf $CONFIGS/vim/colors ~/.vim/
-
--- bin/install.sh
+++ bin/install.sh
@@ -10,10 +10,56 @@ echo -e "${red}" && sleep 0.3
 echo -e "=================================================" 
 echo -e "================= INSTALLING ====================" 
 echo -e "================================================="
-# Configurations __________________________________________#
+echo -e "${reset}"
+
+####################################################
+############### Copying Configrations ##############
+####################################################
+
 BIN=$(find ~ -type d -path "*/myLinux/bin")
 source $BIN/set-configs.sh
-source $BIN/download-packages.sh
 
+####################################################
+############### Installing packages ################
+####################################################
+
+source $BIN/download-packages.sh
 source ~/.bashrc
-echo -e "${reset}"
+
+####################################################
+################## Setting up git ##################
+####################################################
+
+GIT_NAME=$(git config --global user.name)
+GIT_EMAIL=$(git config --global user.email)
+
+# Check if name and email is set
+if [ -z "$GIT_NAME" ] || [ -z "$GIT_EMAIL" ]; then
+    echo "! Git name and email are not set!"
+    read -p "Enter your name: " NEW_NAME
+    read -p "Enter your email: " NEW_EMAIL
+    git config --global user.name "$NEW_NAME"
+    git config --global user.email "$NEW_EMAIL"
+fi
+
+git config --global user.name NEW_NAME 
+git config --global user.email NEW_EMAIL
+
+git config --global init.defaultBranch main
+git config --global core.editor "vim"
+git config --global color.ui auto
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.cm commit
+git config --global alias.st status
+git config --global alias.graph "log --oneline --graph --all --decorate"
+git config --global alias.undo "reset --soft HEAD~1"   # Undo last commit
+git config --global alias.bu "!git commit -am '[backup]' && git push origin main"   
+
+git config --global credential.helper store  # Store Credentials (Avoid Password Prompts)
+
+git config --global core.autocrlf input # Fix Line Ending Issues (For cross-platform development)
+
+
+
